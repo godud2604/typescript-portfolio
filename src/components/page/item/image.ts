9f06223c92fc910e9c23bfbef70f2c6b77519dd6
godud2604 <ees238@naver.com>
2022-03-23 16:44:46 +0900

    🩹 title <p> tag -> <h2> tag

--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -3,13 +3,13 @@ import { BaseComponent } from '../../component.js'
 export class ImageComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
     super(`<section class="image">
-      <div class="image-holder">
-        <div>
-          <img class="image-thumbnail" />
-        </div>
-        <p class="image-title"></p>
-      </div>
-    </section>`)
+            <div class="image-holder">
+              <div>
+                <img class="image-thumbnail" />
+              </div>
+              <h2 class="image-title"></h2>
+            </div>
+          </section>`)
 
     const imageElement = this.element.querySelector(
       '.image-thumbnail'
@@ -19,7 +19,7 @@ export class ImageComponent extends BaseComponent<HTMLElement> {
 
     const titleElement = this.element.querySelector(
       '.image-title'
-    )! as HTMLParagraphElement
+    )! as HTMLHeadingElement
     titleElement.textContent = title
   }
 }
